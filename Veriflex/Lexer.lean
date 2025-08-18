521ab22274b28053b1091101257119639263b42d
David Binder <david.binder@uni-tuebingen.de>
2025-08-18 15:30:46 +0100

    Naming consistency

--- Veriflex/Lexer.lean
+++ Veriflex/Lexer.lean
@@ -9,7 +9,7 @@ This file implements the function `lex` which takes a grammar and lexes a string
 -/
 namespace Veriflex
 
-def maxpref_one_rec (best : Option (List Char × List Char))
+def max_pref_one_rec (best : Option (List Char × List Char))
         (left right : List Char)
         (re : RE)
         : Option (List Char × List Char) :=
@@ -20,8 +20,8 @@ def maxpref_one_rec (best : Option (List Char × List Char))
   | s :: right' => let re' := derivative s re
                let left' := left ++ [s]
                if nullable re'
-               then maxpref_one_rec (some (left', right')) left' right' re'
-               else maxpref_one_rec best left' right' re'
+               then max_pref_one_rec (some (left', right')) left' right' re'
+               else max_pref_one_rec best left' right' re'
 
 
 /--
@@ -29,18 +29,19 @@ Given a string and a rule, compute the longest prefix that matches this rule.
 If the regular expression matches successfully, return the computed token,
 the length of the consumed input, and the remainder of the output.
 -/
-def maxpref_one {tok : Type}(s : List Char) (r : Rule tok) : Option (tok × Nat × List Char) :=
-  match maxpref_one_rec none [] s r.re with
+def max_pref_one {tok : Type}(s : List Char) (r : Rule tok) : Option (tok × Nat × List Char) :=
+  match max_pref_one_rec none [] s r.re with
   | none => none
   | some (pre, rest) => some (r.action (String.mk pre), pre.length, rest)
 
 
 theorem max_pref_one_smaller :
-  maxpref_one input rule = some ⟨tok, n, rest⟩ →
-  rest.length < input.length := sorry
+  max_pref_one input rule = some ⟨tok, n, rest⟩ →
+  rest.length < input.length := by
+  sorry
 
 def max_pref {tok : Type}(input : List Char) (rules : List (Rule tok)) : Option (tok × List Char) :=
-  let max_prefixes : List (Option (tok × Nat × List Char)) := rules.map (λ rule => maxpref_one input rule)
+  let max_prefixes : List (Option (tok × Nat × List Char)) := rules.map (λ rule => max_pref_one input rule)
   let max_prefixes : List (tok × Nat × List Char) := max_prefixes.filterMap id
   let result : Option (tok × Nat × List Char):= find_first_max max_prefixes (λ x => x.2.1)
   result.map (λ x => ⟨x.1, x.2.2 ⟩)
